a0cf15598a2f8b9c50716b75e27a4ced6ffc8d80
System Administrator <837244202@qq.com>
2021-04-21 22:42:17 +0800

    Site updated: 2021-04-21 22:42:17

--- photos/ins.js
+++ photos/ins.js
@@ -117,8 +117,8 @@
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
           // 需要修改https://raw.githubusercontent.com/你的github名/你的github仓库名/master/min_photos/即可
-          var minSrc = 'https://github.com/zfl837244202/blog_photos/tree/master/min_photos/' + data.link[i];
-          var src = 'https://github.com/zfl837244202/blog_photos/tree/master/photos' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/zfl837244202/blog_photos/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/zfl837244202/blog_photos/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
